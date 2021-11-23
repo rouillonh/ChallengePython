d3bb9eacbdb517e60821deea858f4d7aaaa2b5ee
rouillonh <gaxf100@hotmail.com>
2021-11-23 04:51:29 -0500

    primer challenge

---
+++ challenge1_rouillonh.py
@@ -0,0 +1,7 @@
+print("\tWelcome to the Letter Count App")
+name = input("\nWhat is your name? ").title()
+print("\nHello! ",name)
+print("I will count the number of times that a specific letter occours in a message")
+mens = input("\nPlease enter a message: ")
+letter = input("Which letter would you like to count the occurrences of: ")
+print(name," your message has ",mens.count(letter)," ",letter,"'s in it")
